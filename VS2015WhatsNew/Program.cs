3b943f4609dc469027490353ed09b7ef47120881
Guy Bertental <guybartal@gmail.com>
2016-06-19 21:32:54 +0300

    add breakpoint demo

--- VS2015WhatsNew/Program.cs
+++ VS2015WhatsNew/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,6 @@ namespace VS2015WhatsNew
 {
     class Program
     {
-        public class Person
-        {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-
-        }
-
         public class Fibonachi
         {
             // 1,1,2,3,5,...
@@ -34,12 +28,30 @@ namespace VS2015WhatsNew
             }
         }
 
+        public class StringUtilities
+        {
+            public static string ReverseString(string str)
+            {
+                // where's the bug in my code?
+                int len = str.Length;
+                if (len > 1)
+                {
+                    var first = str.Substring(1);
+                    var last = str.Substring(1, 1);
+                    var reverse = ReverseString(first) + last;
+
+                    return reverse;
+                }
+                return str;
+            }
+        }
+
         public class MemLoad
         {
             Dictionary<Guid, byte[]> repo = new Dictionary<Guid, byte[]>();
-            public void Load()
+            public void Load(int count)
             {
-                for (int i=0; i<5000; i++)
+                for (int i=0; i<count; i++)
                 {
                     repo.Add(Guid.NewGuid(),System.IO.File.ReadAllBytes("guy.jpg"));
                     System.Threading.Thread.Sleep(10);
@@ -51,20 +63,27 @@ namespace VS2015WhatsNew
 
         static void Main(string[] args)
         {
-            Fibonachi f = new Fibonachi();
-            int index = 4;
-            int result = f.fibo(index);
-            Console.WriteLine($"fibo({index}) = {result}");
+            //Fibonachi f = new Fibonachi();
+            //int index = 4;
+            //int result = f.fibo(index);
+            //Console.WriteLine($"fibo({index}) = {result}");
+
+            var str = "hello SDP audience!";
+            var revStr = StringUtilities.ReverseString("hello SDP audience!");
+            Console.WriteLine($"{str} => {revStr}");
 
             //try
             //{
             //    MemLoad memLoad = new MemLoad();
-            //    memLoad.Load();
+            //    memLoad.Load(1000);
             //}
-            //catch(Exception ex)
+            //catch (Exception ex)
             //{
             //    Console.WriteLine(ex.ToString());
             //}
+
+
+
 
             Console.ReadKey();
 
